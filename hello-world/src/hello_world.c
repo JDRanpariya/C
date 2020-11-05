b198668efef31efda6ef8987f3601939836bdff3
jdranpariya <jdpatel.code@gmail.com>
2020-11-05 12:29:28 +0530

    segmentation fault occured

--- hello-world/src/hello_world.c
+++ hello-world/src/hello_world.c
@@ -10,6 +10,6 @@ const char *hello(void)
 {
    // To fix this function, change the return statement here to instead return
    // a string equivalent to the string expected by the failing test.
-   printf("Hello, World!");
-   return 0;
+   hello();
+   return NULL;
 }
