4a92d79b0c5e48080e9a5ff1eb30fc7edeb76258
jdranpariya <jdpatel.code@gmail.com>
2020-11-05 19:57:54 +0530

    3 errors

--- armstrong-numbers/src/armstrong_numbers.c
+++ armstrong-numbers/src/armstrong_numbers.c
@@ -3,16 +3,23 @@
 
 bool is_armstrong_number(int num) {
 
-    int n1,count=1,result;
+    int n1,count=0,result=0,n2;
 
     n1 = num;
-
+    n2 = num;
+/* the following loop counts the digits */
     while(n1 != 0){
         n1 = n1/10;
-        result += count*(num % 10);
         count+=1;
     }
+printf("%d", count);
+/* this loop add each count*digit to result */
+    while(n2 != 0){
+        result += count*(n2 % 10);
+        n2 = n2/10;
+    }
 
+/* the following conditional checks if it's a armstrong_number */
     if ( result == num ){
         return true; 
     }
