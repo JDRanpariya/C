9eba7fd0f0c65fd0e1b219a1ae744fe3ed97c7e4
jdranpariya <jdpatel.code@gmail.com>
2020-11-06 09:56:35 +0530

    code refactored

--- armstrong-numbers/src/armstrong_numbers.c
+++ armstrong-numbers/src/armstrong_numbers.c
@@ -4,29 +4,23 @@
 
 bool is_armstrong_number(int num) {
 
-    int n1,count=0,result=0,n2;
+    int count=0;
 
-    n1 = num;
-    n2 = num;
 /* the following loop counts the digits */
-    while(n1 != 0){
-        n1 = n1/10;
-        count+=1;
+    for(int n1=num; n1!=0; n1 /= 10){
+        count += 1;
     }
-/* this loop add each count*digit to result */
+
+    int result=0,n2=num;
+
+/* this loop add each digit raised to the power of count( ie: digit^count ) to result */
     while(n2 != 0){
         result += pow(n2 % 10, count);
-        n2 = n2/10;
+        n2 /= 10;
     }
 
-/* the following conditional checks if it's a armstrong_number */
-    if ( result == num ){
-        return true; 
-    }
-    else{
-       return false; 
-    }
-
+/* the following line returns true if it's a armstrong_number and vice-versa */
+    return result == num;
 }
 
 
